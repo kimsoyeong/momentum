69930b8c8500ad2fe91053a06977e4f67a1e81fb
kimsoyeong <iloveit356@gmail.com>
2022-01-04 21:01:51 +0900

    feat: timer

--- js/timer.js
+++ js/timer.js
@@ -1,14 +1,37 @@
 const timerSpan = document.querySelector("span#timer-span");
+const startBtn = document.querySelector("#start-btn");
+const stopBtn = document.querySelector("#stop-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
-function timerCount() {
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+let TIME = 0;
+
+function updateTimer() {
+    const hours =  String(Math.floor(TIME/3600)).padStart(2, "0");
+    const minutes =  String(Math.floor(TIME/60) % 60).padStart(2, "0");
+    const seconds =  String(TIME % 60).padStart(2, "0");
 
     timerSpan.innerText = `${hours}:${minutes}:${seconds}`;
+
+    TIME++;
 }
 
-// theDay.innerText = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
+let cron;
 
-setInterval(timerCount, 1000);
+function startTimer(){
+    updateTimer();
+    cron = setInterval(updateTimer, 1000);
+}
+
+function stopTimer(){
+    clearInterval(cron);
+}
+
+function resetTimer(){
+    stopTimer();
+    timerSpan.innerText = "00:00:00";
+    TIME = 0;
+}
+
+startBtn.addEventListener("click", startTimer);
+stopBtn.addEventListener("click", stopTimer);
+resetBtn.addEventListener("click", resetTimer);
